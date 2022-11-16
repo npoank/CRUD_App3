47958a645ee152602ce10a632d1180e0d38af5ec
Kulbicki Andrey <kubl-ml@mail.ru>
2022-11-16 20:31:15 +0300

    JDBC with postgre

---
+++ src/main/java/org/andrew/crud/dao/PersonDAO.java
@@ -0,0 +1,86 @@
+package org.andrew.crud.dao;
+
+import org.andrew.crud.models.Person;
+import org.springframework.stereotype.Component;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "1410";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()){
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return people;
+    }
+
+    public Person show(int id) {
+        // return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
+    }
+
+    public void save(Person person) {
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() +
+                    "'," + person.getAge() + ",'" + person.getEmail() +"')";
+            statement.executeUpdate(SQL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(int id, Person updatedPerson) {
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+//        people.removeIf(p -> p.getId() == id);
+    }
+}
---
+++ src/main/java/org/andrew/crud/models/Person.java
@@ -0,0 +1,64 @@
+package org.andrew.crud.models;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name must not be empty")
+    @Size(min = 2, max = 30, message = "Invalid name size (2-30 symbols)")
+    private String name;
+
+    @Min(value = 0, message = "Minimal age 0")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Invalid email")
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
